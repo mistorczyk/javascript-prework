eec4d9338dbaac1b4f881cff23bfe685ae99e18d
mistorczyk <75639021+mistorczyk@users.noreply.github.com>
2024-02-01 12:17:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = 'kamień';
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
