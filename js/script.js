d30174c43a1300841c3b84036e922ef365d8248a
mistorczyk <75639021+mistorczyk@users.noreply.github.com>
2024-02-05 14:28:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput == 2){ 
+  playerMove = 'papier';
+}
+else (playerInput == 3){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if (randomNumber == 2){ 
+  computerMove = 'papier';
+}
+else (randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
